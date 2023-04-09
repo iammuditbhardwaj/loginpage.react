1fd562d33be3b4891e5ac508744adf415c243537
Mudit Bhardwaj <126606748+iammuditbhardwaj@users.noreply.github.com>
2023-04-09 22:07:41 +0530

    Update and rename README.md to App.js

---
+++ App.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Email:', email);
+    console.log('Password:', password);
+  }
+
+  return (
+    <div className="App">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
